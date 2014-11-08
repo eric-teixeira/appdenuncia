8ad6f43499bcaae63b33c3b4d3309b53f33ec91b
Eric Teixeira <e.teixeira1994@gmail.com>
2014-11-07 22:26:39 -0200

    Inserindo o bootstrap

--- application/views/header.php
+++ application/views/header.php
@@ -3,7 +3,14 @@
     <head>
         <meta charset="UTF-8">
         <title>APP Denúncia</title>
-        <link rel='stylesheet' type='text/css' href="<?php echo base_url(); ?>assets/css/style.css">
+        <!-- CSS -->
+        <link href="<?php echo base_url(); ?>assets/css/style.css" rel='stylesheet' type='text/css'>
+        <link href="<?php echo base_url(); ?>assets/css/bootstrap.min.css" rel="stylesheet">
+        <link rel="shortcut icon" href="favicon.ico">
+        
+        <!-- JS -->
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+        <script src="<?php echo base_url(); ?>assets/js/bootstrap.min.js"></script>
     </head>
     <body>
         <h1>App Denúncia</h1>
